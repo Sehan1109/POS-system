2918e4ba9f73fa175aa03a2bd9f64161d22e5eab
Sehan1109 <2022t01533@stu.cmb.ac.lk>
2026-04-22 15:58:25 +0530

    fix: dropdown not working due to missing Alpine.js

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -45,8 +45,7 @@ new class extends Component {
                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
 
                                 <!-- FIXED -->
-                                <div x-data="{{ json_encode(['name' => auth()->user()?->name]) }}" x-text="name">
-                                </div>
+                                <div x-data="{ name: '{{ auth()->user()?->name }}' }" x-text="name"></div>
 
                                 <div class="ms-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
